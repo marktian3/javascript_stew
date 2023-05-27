3bb773708937108bc9cbdc3f181c755d0e972560
mark <marktian3@gmail.com>
2023-05-27 15:26:47 -0600

    Make fully functioning game with UI

--- rock-paper-scissors/rps.js
+++ rock-paper-scissors/rps.js
@@ -1,43 +1,65 @@
 function getComputerChoice() {
-  choices = ['rock', 'paper', 'scissors'];
-  random_index = Math.floor(Math.random() *3);
+  let choices = ['rock', 'paper', 'scissors'];
+  let random_index = Math.floor(Math.random() *3); 
+  document.getElementById("c-choice").innerHTML = "Computer chose: " + choices[random_index];
   return choices[random_index];
 }
 
 function playRound(playerChoice, computerChoice) {
-
+  console.log("Playing a round");
   if (playerChoice == computerChoice) {
-    return "It is a tie";
+    tie();
   } else if (playerChoice == 'rock' && computerChoice == 'scissors') {
-    return "You win! Rock beats scissors";
+    win();
   } else if (playerChoice == 'paper' && computerChoice == 'rock') {
-    return "You win! Paper beats scissors";
+    win();
   } else if (playerChoice == 'scissors' && computerChoice == 'paper') {
-    return "You win!, Scissors beats paper";
+    win();
   } else {
-    return "You lose! " + playerChoice + " loses to " + computerChoice;
+    lose();
   }
-
 }
 
-// function playGame(rounds) {
-
-//   for (let i = 0; i < rounds; i++){
-//     let playerChoice = prompt("Please enter your selection: (rock, paper, or scissors)");
-//     playerChoice = playerChoice.toLowerCase();
-//     let computerChoice = getComputerChoice();
-//     console.log(playRound(playerChoice, computerChoice));
-//   }
-// }
-
-function startGame() {
-  document.getElementById("start-button").remove();
-  document.getElementById("rps-game").style.display = "";
+function tie() {
+  document.getElementById("result").innerHTML = "It is a tie";
 }
 
-const startButton = document.getElementById("start-button");
-startButton.addEventListener("click", startGame);
-element.addEventListener("click", startGame);
+function win() {
+  document.getElementById("result").innerHTML = "You win!";
+  document.getElementById("player-win-count").innerHTML++;
+  if (document.getElementById("player-win-count").innerHTML == 5) {
+    document.getElementById("result").innerHTML = "You have reached 5 first! You are a winner!!";
+    showRestartGameButton();
+  }
+}
 
+function lose() {
+  document.getElementById("result").innerHTML = "You lose";
+  document.getElementById("computer-win-count").innerHTML++;
+  if (document.getElementById("computer-win-count").innerHTML == 5) {
+    document.getElementById("result").innerHTML = "Your opponent has reached 5 first! You are a loser";
+    showRestartGameButton();
+  }
+}
 
-// playGame(5);
+function showRestartGameButton() {
+  document.getElementById("restart").style.display = "";
+}
+
+function restartGame() {
+  document.getElementById("player-win-count").innerHTML = 0;
+  document.getElementById("computer-win-count").innerHTML = 0;
+  document.getElementById("result").innerHTML = "";
+  document.getElementById("restart").style.display = "none";
+  document.getElementById("c-choice").innerHTML = "";
+}
+
+const rock_button = document.getElementById("selection-rock");
+const paper_button = document.getElementById("selection-paper");
+const scissors_button = document.getElementById("selection-scissors");
+rock_button.addEventListener("click", () => { playRound("rock", getComputerChoice()) });
+paper_button.addEventListener("click", () => { playRound("paper", getComputerChoice()) });
+scissors_button.addEventListener("click", () => { playRound("scissors", getComputerChoice()) });
+
+const restart_button = document.getElementById("restart");
+restart_button.addEventListener("click", restartGame);
